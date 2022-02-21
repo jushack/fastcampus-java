aff2413f578be0e6884d1129c6a872aac55e2251
dotorimookj <dotorimookj@gmail.com>
2022-02-21 16:42:18 +0900

    first commit

---
+++ src/Ch01/ch08.java
@@ -0,0 +1,26 @@
+package Ch01;
+
+public class ch08 {
+
+	public static void main(String[] args) {
+		
+		char ch1 = 'A';
+		System.out.println(ch1);
+		System.out.println((int)ch1);
+	
+		char ch2 = 66;
+		System.out.println(ch2);
+		
+		int ch3 = 67;
+		System.out.println(ch3);
+		System.out.println((char)ch3);
+		
+		char ch5 = 'วั';
+		char ch6 ='\uD55C';
+		System.out.println(ch5);
+		System.out.println(ch6);
+	
+	
+	}
+
+}
