8b7a39bb6d526a02e4c84faaa1856533af232821
jushack <3.jushack.1@gmail.com>
2022-03-07 15:12:52 +0900

    ch10

---
+++ src/Ch10/BirthDayTest.java
@@ -0,0 +1,17 @@
+package Ch10;
+
+public class BirthDayTest {
+
+	public static void main(String[] args) {
+		
+		BirthDay date = new BirthDay();
+		
+		date.setYear(2022);
+		date.setMonth(5);
+		date.setDay(2);
+		
+		date.showDate();
+		
+	}
+
+}
