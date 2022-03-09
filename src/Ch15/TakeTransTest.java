3a340e2c1b41887d352efbbe14052526e690e728
jushack <3.jushack.1@gmail.com>
2022-03-09 17:56:51 +0900

    ch15

---
+++ src/Ch15/TakeTransTest.java
@@ -0,0 +1,16 @@
+package Ch15;
+
+public class TakeTransTest {
+
+	public static void main(String[] args) {
+		
+		Student studentE = new Student("Edward", 20000);
+		Taxi wellTaxi = new Taxi("잘 간다 운수");
+		studentE.takeTaxi(wellTaxi);
+		
+		studentE.showInfo();
+		wellTaxi.showTaxiInfo();
+		
+	}
+
+}
